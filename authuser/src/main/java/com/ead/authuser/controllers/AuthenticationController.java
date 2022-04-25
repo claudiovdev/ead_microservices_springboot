1f4153830c0f8107bbaec6e7fb444ecb3691031e
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2022-04-25 11:47:53 -0300

    Criando metodos de atualização do password, imagem e geral

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,7 +26,8 @@ public class AuthenticationController {
     UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Object> registerUser(@RequestBody UserDto userDto){
+    public ResponseEntity<Object> registerUser(@RequestBody
+                                                  @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto){
 
         //Realizar verificação antes de cadastrar um novo estudante!!!
         if(userService.existsByUsername(userDto.getUsername())){
