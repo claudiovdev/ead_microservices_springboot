d0bf451f7ab98c6ae27728562507bb3d81a65728
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2022-05-07 16:43:26 -0300

    FetchType, FetchMode e @Query

--- course/src/main/java/com/ead/course/models/LessonModel.java
+++ course/src/main/java/com/ead/course/models/LessonModel.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -33,5 +35,6 @@ public class LessonModel implements Serializable {
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @ManyToOne(optional = false)
+    @Fetch(FetchMode.SUBSELECT)
     private ModuleModel module;
 }
